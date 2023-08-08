ae6d8c3e39d148d00e990d2923915e7b4d97d881
STPV <steve.milan7@gmail.com>
2023-08-08 18:11:01 -0500

    @Param common

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+
+    private cars = ["Honda", "Toyota", "Jeep"];
     
     @Get()
     getAllCars() {
-        return ["Honda", "Toyota"];
+        return this.cars;
+    }
+
+    @Get(':id')
+    getCarById(@Param('id') id : string) {
+                
+        return this.cars[id];
     }
 }
